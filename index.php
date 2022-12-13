ef0918e6ce2c6a6325a3777252083770af998246
GameLineYT <josedaniel.conta@outlook.com>
2022-12-13 15:21:20 -0300

    1

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Midnight Club 3: Dub Edition</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body style="background-color: black;">
+  <br>
+    <div class="d-flex p-2 justify-content-center align-items-center">
+        <a href="#"><img src="media/midnight.png" class="img-fluid m-3" width="380" height="200"></a>
+        <a href="#"><button type="button" class="btn btn-danger p-3 m-3">Login</button></a>
+        <a href="#"><button type="button" class="btn btn-danger p-3 m-3">Register</button></a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCEU7JQsgwMI4l_VUILhIiEQ"><button type="button" class="btn btn-danger p-3 m-3">Youtube Channel</button></a>
+    </div>
+
+<br><br><br>
+
+    <div class="d-flex p-2 justify-content-center align-items-center">
+
+    <div class="card px-2 m-3" style="width: 18rem;">
+    <img src="media/ps2-cover.png" class="card-img-top">
+    <div class="card-body">
+    <h5 class="card-title">PS2 Version</h5>
+    <p class="card-text">Play the Game in your PS2 or in a PS2 Emulator.</p>
+    <a href="#" class="btn btn-success">Download</a>
+    </div>
+    </div>
+    <div class="card m-3" style="width: 18rem;">
+    <img src="media/psp-cover.jpg" class="card-img-top px-5" height="270">
+    <div class="card-body">
+    <h5 class="card-title">PSP Version</h5>
+    <p class="card-text">Play the Game in your PSP or in a PSP Emulator.</p>
+    <a href="#" class="btn btn-success">Download</a>
+    </div>
+    </div>
+
+    <div class="card m-3" style="width: 18rem;">
+    <img src="media/xbox-cover.png" class="card-img-top px-5" height="270">
+    <div class="card-body">
+    <h5 class="card-title">XBOX Version</h5>
+    <p class="card-text">Play the Game in your XBOX or in a XBOX Emulator.</p>
+    <a href="#" class="btn btn-success">Download</a>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="d-flex p-2 justify-content-center align-items-center">
+    <iframe class="m-3" width="800" height="400" src="https://www.youtube.com/embed/eivnVc7yWuY" title="Midnight Club 3: DUB Edition Xbox Trailer - Trailer 4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h3 class="m-3" style="color: silver;">Midnight Club 3: DUB Edition é um jogo eletrônico de corrida<br> desenvolvido pela Rockstar San Diego e publicado pela Rockstar Games.<br> Foi lançado em abril de 2005 para PlayStation 2 e Xbox. Um porte para PlayStation Portable, desenvolvido pela Rockstar Leeds, foi lançado em junho de 2005.<br><br><br>Source: Wikipédia</h3>
+    </div>
+<br><br><br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
